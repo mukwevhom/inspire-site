e127c16ae4115d49b92d1fa4d1ef19bc41ed8d64
Murendeni Mukwevho <mukwevhom@live.com>
2020-04-12 19:52:02 +0200

    Added Gtag Manager

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,15 @@ export default function Layout(props) {
             <Head>
                 <title>Inspire Academy</title>
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+                {/* <!-- Google Tag Manager --> */}
+                <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-NWHXW6Q');`}}/>
+                {/* <!-- End Google Tag Manager --> */}
             </Head>
+            <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NWHXW6Q" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
             <Header />
             {props.children}
             <Connect/>
